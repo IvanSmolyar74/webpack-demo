f4a0444089e8467dbf66394384b195ef4eff5aab
@IvanSmolyar74 <ivansmolar09001@gmail.com>
2021-06-13 23:55:54 +0500

    extracting parts configuration of webpack

---
+++ webpack.parts.js
@@ -0,0 +1,23 @@
+const { MiniHtmlWebpackPlugin } = require('mini-html-webpack-plugin')
+const { WebpackPluginServe } = require('webpack-plugin-serve')
+const { plugins } = require('./webpack.config')
+
+exports.devServer = () => ({
+  watch: true,
+  plugins: [
+    new WebpackPluginServe({
+      port: process.env.PORT || 8080,
+      host: 'localhost',
+      open: true,
+      static: './dist',
+      liveReload: true,
+      waitForBuild: true
+    })
+  ]
+})
+
+exports.page = ({ title }) => ({
+  plugins: [
+    new MiniHtmlWebpackPlugin({ context: { title } }),
+  ]
+})
