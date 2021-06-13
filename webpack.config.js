33792e6a4fb643e1070742285ed213f07be655f2
@IvanSmolyar74 <ivansmolar09001@gmail.com>
2021-06-13 21:31:12 +0500

    add config for webpack-plugin-serve

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const { mode } = require('webpack-nano/argv')
 const { MiniHtmlWebpackPlugin } = require('mini-html-webpack-plugin')
+const { WebpackPluginServe } = require('webpack-plugin-serve')
 
 module.exports = {
   mode,
   plugins: [
-    new MiniHtmlWebpackPlugin({ context: { title: 'Demo' } })
+    new MiniHtmlWebpackPlugin({ context: { title: 'Demo' } }),
+    new WebpackPluginServe({
+      port: process.env.PORT || 8080,
+      static: './dist',
+      liveReload: true,
+      waitForBuild: true
+    })
   ]
 }
